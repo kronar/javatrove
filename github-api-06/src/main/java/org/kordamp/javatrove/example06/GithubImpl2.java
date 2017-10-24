f62f2b5f68a52dd48b2e82a7b89bbba4ff27c83a
nikita.makarov <nikita.makarov@odnoklassniki.ru>
2017-10-24 14:30:34 +0300

    FIT files
    Signed-off-by: nikita.makarov <nikita.makarov@odnoklassniki.ru>

--- github-api-06/src/main/java/org/kordamp/javatrove/example06/GithubImpl2.java
+++ github-api-06/src/main/java/org/kordamp/javatrove/example06/GithubImpl2.java
@@ -3,21 +3,20 @@ package org.kordamp.javatrove.example06;
 import org.kordamp.javatrove.example06.model.Repository;
 import org.kordamp.javatrove.example06.service.Github;
 
-import java.io.IOException;
 import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 
 /**
- * Created by nikita on 05.09.17.
+ * Copy code below with imports section into FIT file githubFIT.impl
  */
 public class GithubImpl2 implements Github {
     @Override
     public CompletableFuture<Collection<Repository>> repositories(String organization) {
         try {
-            Thread.sleep(5000);
+            throw new RuntimeException("GitHub is down");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        throw new IOException("gitHub is down!!!! ");
+        throw new IllegalStateException("Github is down");
     }
 }
